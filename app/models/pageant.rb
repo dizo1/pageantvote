4119b3a60908c4648d673fc63c4e777197df167a
Muganda <mugandadizo@gmail.com>
2017-05-25 04:44:12 +0000

    Timezone render

--- app/models/pageant.rb
+++ app/models/pageant.rb
@@ -3,13 +3,11 @@ class Pageant < ApplicationRecord
     validates :pageant_name, presence: true, length: { minimum: 5, maximum: 30 }
     validates :pageant_description, presence: true, length: { minimum: 5, maximum: 150 }
     validates :pageant_category, presence: true, length: { minimum: 5, maximum: 30 }
-    validates_inclusion_of :time_zone, in: ActiveSupport::TimeZone.all
+    #validates_inclusion_of :time_zone, in: ActiveSupport::TimeZone.all(&:to_s)
     
     
     default_scope -> {order(updated_at: :desc)}
     
     has_many :contestants
-    
-    
     
 end
