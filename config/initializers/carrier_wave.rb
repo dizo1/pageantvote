83f23c489752342be419c414d9a30db725752118
Muganda <mugandadizo@gmail.com>
2017-05-26 13:54:20 +0000

    SEC_KEY & ACCESS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
 CarrierWave.configure do |config|
 config.fog_credentials = {
 :provider => 'AWS',
-:aws_access_key_id => ENV['AKIAJYB4X4PJ6ETKB65A '],
-:aws_secret_access_key => ENV['#dizo@me1'],
+:aws_access_key_id => ENV['AKIAJFSJDJWTV744QZMA'],
+:aws_secret_access_key => ENV['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'],
 :region => ENV['Ohio'] #This is usually required for European Users not all
 }
 config.fog_directory = ENV['prodapp']
