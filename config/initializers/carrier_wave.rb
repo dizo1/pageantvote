827613650ac80b0af81d80741d7d9df87e223616
Muganda <mugandadizo@gmail.com>
2017-05-26 15:26:18 +0000

    set region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ config.fog_credentials = {
 :provider => 'AWS',
 :aws_access_key_id => ENV['S3_ACCESS_KEY'],
 :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-:region => ENV['Ohio'] #This is usually required for European Users not all
+:region => ENV['us-east-2'] #This is usually required for European Users not all
 }
 config.fog_directory = ENV['S3_BUCKET']
 end
