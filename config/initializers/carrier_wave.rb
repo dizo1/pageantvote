966a1cee5c0d4412b236fb7e6cdd91300001571e
Muganda <mugandadizo@gmail.com>
2017-05-26 14:02:23 +0000

    S3 Keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
 CarrierWave.configure do |config|
 config.fog_credentials = {
 :provider => 'AWS',
-:aws_access_key_id => ENV['AKIAJFSJDJWTV744QZMA'],
-:aws_secret_access_key => ENV['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'],
+:aws_access_key_id => ENV['S3_ACCESS_KEY'],
+:aws_secret_access_key => ENV['S3_SECRET_KEY'],
 :region => ENV['Ohio'] #This is usually required for European Users not all
 }
-config.fog_directory = ENV['prodapp']
+config.fog_directory = ENV['S3_BUCKET']
 end
 end
